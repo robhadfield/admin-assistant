1dd2dfb851e01f7bb58c096515ea0d1e3cfefb4a
Rob Hadfield <rhadfield@slido.com>
2022-06-28 13:55:38 +0100

    omni list

--- src/components/Omni.jsx
+++ src/components/Omni.jsx
@@ -12,6 +12,23 @@ export const Omni = ({active}) => {
       {active && 
         <input autoFocus className={classes.input} type="text" placeholder="Start typing..." />
       }
+      <div className={classes.omniList}>
+        <div className={classes.omniListItem}>
+          <div className={classes.omniListItemIcon}>
+            <i className="far fa-circle-s" />
+          </div> Create a new Slido
+        </div>
+        <div className={classes.omniListItem}>
+          <div className={classes.omniListItemIcon}>
+            <i className="far fa-user" />
+          </div> Add a new team member
+        </div>
+        <div className={classes.omniListItem}>
+          <div className={classes.omniListItemIcon}>
+            <i className="far fa-file-invoice" />
+          </div> View invoices
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/styles/omni.js
+++ src/components/styles/omni.js
@@ -3,21 +3,22 @@
 export const omni = {
   inactive: {
     position: 'fixed',
-    transform: 'translate(-50%, -30%)',
+    transform: 'translate(-50%, 40px)',
     opacity: 0
   },
   outer: {
     position: 'fixed',
     left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
+    top: 200,
+    transform: 'translate(-50%, 0)',
     opacity: 1,
     width: 600,
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     padding: 56,
     background: 'white',
     padding: 24,
+    fontFamily: '"Inter", sans-serif',
     borderRadius: 16,
     boxShadow: '0 24px 48px rgba(0,0,0,0.2), 0 0 1px 0 rgba(0,0,0,0.1)',
     transition: 'opacity, transform 300ms ease-out'
@@ -29,10 +30,32 @@ export const omni = {
     fontSize: 24,
     fontFamily: '"Inter", sans-serif',
     border: '2px solid transparent',
-    width: '100%',
 
     '&:focus, &:active, &:hover': {
-      border: '2px solid #24A148'
+      border: '2px solid #198038',
+      outline: 'none'
     }
+  },
+
+
+  omniList: {
+    marginTop: 8
+  },
+  omniListItem: {
+    display: 'flex',
+    padding: 16,
+    fontSize: 14,
+    background: '#f5f5f5',
+    borderRadius: 4,
+    marginBottom: 2,
+
+    '&:hover': {
+      background: '#f1f1f1',
+      cursor: 'pointer'
+    }
+  },
+  omniListItemIcon: {
+    opacity: '0.8',
+    marginRight: 12
   }
 }
